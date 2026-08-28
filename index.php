cab31a6dd05c7ce289a3dc11abc3f9cdd23720b9
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-29 00:15:04 +0800

    Test show result and add temporary style

--- index.php
+++ index.php
@@ -4,21 +4,33 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact Form</title>
-  <link rel="stylesheet" href="/php-contact-form/assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
-  <section class="form-cotainer">
+  <section class="form-container">
 
-    <form action="" method="post">
+    <h1>Contact Form</h1>
+
+    <form action="" method="post" class="form">
       <label for="name">Name: </label>
       <input type="text" name="name" id="name">
       <label for="email">Email: </label>
       <input type="email" name="email" id="email">
       <label for="message">Message: </label>
-      <textarea name="message" id="message">Type your message here...</textarea>
+      <textarea name="message" id="message" placeholder="Type your message here..." rows="5"></textarea>
       <input type="submit" value="Submit">
     </form>
+
+    <?php
+
+      if(isset($_POST["name"]) && isset($_POST["email"])) {
+        echo "Name: " . $_POST["name"] . "<br>"
+        . "Email: " . $_POST["email"] . "<br>"
+        . "Message: " . $_POST["message"];
+      }
+
+    ?>
 
   </section>
 
