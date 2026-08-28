18fedf789fbebb75d976c78236836b2b6c861429
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-08-28 23:36:30 +0800

    Add html form

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Form</title>
+  <link rel="stylesheet" href="/php-contact-form/assets/css/style.css">
+</head>
+<body>
+
+  <section class="form-cotainer">
+
+    <form action="" method="post">
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name">
+      <label for="email">Email: </label>
+      <input type="email" name="email" id="email">
+      <label for="message">Message: </label>
+      <textarea name="message" id="message">Type your message here...</textarea>
+      <input type="submit" value="Submit">
+    </form>
+
+  </section>
+
+</body>
+</html>
